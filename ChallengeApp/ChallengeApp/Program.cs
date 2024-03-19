9ba1dca98c9ffe92b5a2de21e743a15196307679
Marta Kosmowska <kosmowskam@gmail.com>
2024-03-19 08:44:02 +0100

    dzien 6

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,74 +1,16 @@
-﻿int number = 1234567890;
-string numberInString = number.ToString();
-char[] signs = numberInString.ToArray();
+﻿
+using ChallengeApp;
 
+User user1 = new User("Adam", "pipipi");
+User user2 = new User("Zuzia", "pipipi");
+User user3 = new User("Kamil", "pipipi");
+User user4 = new User("Damian", "pipipi");
 
-int counter0 = 0;
-int counter1 = 0;
-int counter2 = 0;
-int counter3 = 0;
-int counter4 = 0;
-int counter5 = 0;
-int counter6 = 0;
-int counter7 = 0;
-int counter8 = 0;
-int counter9 = 0;
+user1.AddScore(5);
+user1.AddScore(2);
+user1.AddScore(10);
+var result = user1.Result;
+Console.WriteLine(result);
 
-
-foreach (char sign in signs)
-{
-    if (sign == '0')
-    {
-        counter0++;
-    }
-    else if (sign == '1')
-    {
-        counter1++;
-    }
-    else if (sign == '2')
-    {
-        counter2++;
-    }
-    else if (sign == '3')
-    {
-        counter3++;
-    }
-    else if (sign == '4')
-    {
-        counter4++;
-    }
-    else if (sign == '5')
-    {
-        counter5++;
-    }
-    else if (sign == '6')
-    {
-        counter6++;
-    }
-    else if (sign == '7')
-    {
-        counter7++;
-    }
-    else if (sign == '8')
-    {
-        counter8++;
-    }
-    else
-    {
-        counter9++;
-    }
-
-}
-Console.WriteLine("Wynik dla liczby:"+number);
-Console.WriteLine("0=>" + counter0);
-Console.WriteLine("1=>" + counter1);
-Console.WriteLine("2=>" + counter2);
-Console.WriteLine("3=>" + counter3);
-Console.WriteLine("4=>" + counter4);
-Console.WriteLine("5=>" + counter5);
-Console.WriteLine("6=>" + counter6);
-Console.WriteLine("7=>" + counter7);
-Console.WriteLine("8=>" + counter8);
-Console.WriteLine("9=>" + counter9);
 
 
